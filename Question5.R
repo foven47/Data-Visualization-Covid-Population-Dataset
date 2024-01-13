b8bdc374715635e721c0b69a6222d8a323a9cef9
jiersss <nrhazira15@gmail.com>
2024-01-14 02:14:49 +0800

    update Q5

--- Question5.R
+++ Question5.R
@@ -1,4 +1,4 @@
-
+library(dplyr)
 library(ggplot2)
 
 setwd("C:/Users/hazira/Desktop/assignment2_351")
@@ -35,92 +35,81 @@ population_data_filtered <- population_data_filtered[population_data_filtered$se
 
 # Group by year and ethnicity and sum the population of all state based on ethincity
 population_data_filtered <- summarise(group_by(population_data_filtered, year = format(date, "%Y"), ethnicity ),
-                                      , population = sum(population))
-
-
-# Aggregate the data with overall categories
-new_population_overall <- aggregate(cbind(population_data_filtered$population) ~
-                                      population_data_overall_sex +
-                                      population_data_overall_age ,
-                                    data = population_data_filtered, FUN = sum)
+                               population = sum(population))
 
 # Filter data for year 2020
-ethnicity_composition_20 <- population_data_filtered[format((population_data_filtered$date),"%Y") == "2020",]
+ethnicity_composition_20 <- population_data_filtered[population_data_filtered$year == "2020", ]
 # Filter data for year 2021
-ethnicity_composition_21 <- population_data_filtered[format((population_data_filtered$date),"%Y") == "2021",]
+ethnicity_composition_21 <- population_data_filtered[population_data_filtered$year == "2021", ]
 # Filter data for year 2022
-ethnicity_composition_22 <- population_data_filtered[format((population_data_filtered$date),"%Y") == "2022",]
-# Filter data for year 2021
-ethnicity_composition_23 <- population_data_filtered[format((population_data_filtered$date),"%Y") == "2023",]
-  
-  # Create a bar plot for year 2020
-  ggplot(ethnicity_composition_20, aes(y = population, x = ethnicity, fill = ethnicity)) +
-    geom_bar(stat = "identity", position = "dodge", width = 0.7)+
-    geom_text(
-      aes(label = population),
-      position = position_dodge(width = 1),
-      vjust = 0.5,
-      angle = 90,
-      size = 3,
-      hjust = -0.1
-    ) +
-    labs(title = paste("Ethnicity Composition in year 2020"),
-         x = "Ethnicity",
-         y = "Population ('000')") +
-    theme_minimal() +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1))
-  
-  # Create a bar plot for year 2021
-  ggplot(ethnicity_composition_21, aes(y = population, x = ethnicity, fill = ethnicity)) +
-    geom_bar(stat = "identity", position = "dodge", width = 0.7)+
-    geom_text(
-      aes(label = population),
-      position = position_dodge(width = 1),
-      vjust = 0.5,
-      angle = 90,
-      size = 3,
-      hjust = -0.1
-    ) +
-    labs(title = paste("Ethnicity Composition in year 2021"),
-         x = "Ethnicity",
-         y = "Population ('000')") +
-    theme_minimal() +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1))
-  
-  # Create a bar plot for year 2022
-  ggplot(ethnicity_composition_22, aes(y = population, x = ethnicity, fill = ethnicity)) +
-    geom_bar(stat = "identity", position = "dodge", width = 0.7)+
-    geom_text(
-      aes(label = population),
-      position = position_dodge(width = 1),
-      vjust = 0.5,
-      angle = 90,
-      size = 3,
-      hjust = -0.1
-    ) +
-    labs(title = paste("Ethnicity Composition in year 2022"),
-         x = "Ethnicity",
-         y = "Population ('000')") +
-    theme_minimal() +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1))
-  
-  # Create a bar plot for year 2023
-  ggplot(ethnicity_composition_23, aes(y = population, x = ethnicity, fill = ethnicity)) +
-    geom_bar(stat = "identity", position = "dodge", width = 0.7)+
-    geom_text(
-      aes(label = population),
-      position = position_dodge(width = 1),
-      vjust = 0.5,
-      angle = 90,
-      size = 3,
-      hjust = -0.1
-    ) +
-    labs(title = paste("Ethnicity Composition in year 2023"),
-         x = "Ethnicity",
-         y = "Population ('000')") +
-    theme_minimal() +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ethnicity_composition_22 <- population_data_filtered[population_data_filtered$year == "2022", ]
+# Filter data for year 2023
+ethnicity_composition_23 <- population_data_filtered[population_data_filtered$year == "2023", ]
 
+# Create a bar plot for year 2020
+ggplot(ethnicity_composition_20, aes(y = population, x = ethnicity, fill = ethnicity)) +
+  geom_bar(stat = "identity", position = "dodge", width = 0.7)+
+  geom_text(
+    aes(label = population),
+    position = position_dodge(width = 1),
+    vjust = -0.5,
+    angle = 0,
+    size = 3,
+    hjust = 0.5
+  ) +
+  labs(title = paste("Ethnicity Composition in year 2020"),
+       x = "Ethnicity",
+       y = "Population ('000)") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 40, hjust = 1))
 
+# Create a bar plot for year 2021
+ggplot(ethnicity_composition_21, aes(y = population, x = ethnicity, fill = ethnicity)) +
+  geom_bar(stat = "identity", position = "dodge", width = 0.7)+
+  geom_text(
+    aes(label = population),
+    position = position_dodge(width = 1),
+    vjust = -0.5,
+    angle = 0,
+    size = 3,
+    hjust = 0.5
+  ) +
+  labs(title = paste("Ethnicity Composition in year 2021"),
+       x = "Ethnicity",
+       y = "Population ('000)") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 40, hjust = 1))
 
+# Create a bar plot for year 2022
+ggplot(ethnicity_composition_22, aes(y = population, x = ethnicity, fill = ethnicity)) +
+  geom_bar(stat = "identity", position = "dodge", width = 0.7)+
+  geom_text(
+    aes(label = population),
+    position = position_dodge(width = 1),
+    vjust = -0.5,
+    angle = 0,
+    size = 3,
+    hjust = 0.5
+  ) +
+  labs(title = paste("Ethnicity Composition in year 2022"),
+       x = "Ethnicity",
+       y = "Population ('000)") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 40, hjust = 1))
 
+# Create a bar plot for year 2023
+ggplot(ethnicity_composition_23, aes(y = population, x = ethnicity, fill = ethnicity)) +
+  geom_bar(stat = "identity", position = "dodge", width = 0.7)+
+  geom_text(
+    aes(label = population),
+    position = position_dodge(width = 1),
+    vjust = -0.5,
+    angle = 0,
+    size = 3,
+    hjust = 0.5
+  ) +
+  labs(title = paste("Ethnicity Composition in year 2023"),
+       x = "Ethnicity",
+       y = "Population ('000)") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 40, hjust = 1))
